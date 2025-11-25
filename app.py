46e611d24e5e211d41db1af5502c8330f8d55bd3
mithunkumar23112005 <mithunkumarg2005@gmail.com>
2025-11-26 01:36:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -898,6 +898,7 @@ def on_make_online_move(data):
 # ============================================================
 if __name__ == "__main__":
     print("⚡ Reversi AI Backend (REST + SocketIO) running at http://localhost:5000")
+    port = int(os.environ.get("PORT", 5000))
+    socketio.run(app, host="0.0.0.0", port=port)
 
-    socketio.run(app, host="0.0.0.0", port=5000)
 
